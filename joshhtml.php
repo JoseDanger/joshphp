6c683efb2d41c3dadf66095081319bfdf6ea93d2
JohnnyDanger <joshua.cramond@gmail.com>
2020-11-04 09:00:27 +1300

    created loop in html

--- joshhtml.php
+++ joshhtml.php
@@ -7,5 +7,14 @@
 <body>
 <h1> This is another way to write php</h1>
 <p> I will put my paragraphs below</p>
+<?php
+$i = 0;
+while ($i <10){
+echo "<P> this is the ".$i. "th line</P>php">
+    $i++;
+}
+
+
+?>
 </body>
 </html>
