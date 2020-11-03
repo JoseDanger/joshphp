eebdefd80a15725ebb4b1af69bd58ac119bfce5c
JohnnyDanger <joshua.cramond@gmail.com>
2020-11-04 10:08:02 +1300

    rules to add database follow these rules please

---
+++ PhpandDatabase/firstConnection.php
@@ -0,0 +1,24 @@
+<?php
+//create a database connection
+$server = "http://fnx6frzmhxw45qcb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com/";
+$dusername = "a7vsd5p699o1mif7";
+$dpassword = "uu5y3xzmj399r0ua";
+$dbname = "m2bsi2ekjab5fc3a";
+
+$con = new mysqli($server, $dbusername, $dbpassword $dbdomain);
+
+if ($con->error){
+    echo $conn->error;
+}else {
+    echo "Connected"
+}
+
+// will write a query
+
+//execute the query
+
+// show my result
+
+
+
+?>
