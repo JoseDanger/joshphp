54edee08aa31e5285ebe110018227d6d8f611341
JohnnyDanger <joshua.cramond@gmail.com>
2020-11-05 10:13:30 +1300

    try

--- PhpandDatabase/login.php
+++ PhpandDatabase/login.php
@@ -5,3 +5,17 @@ $password = $_POST["password"];
 
     echo "username:".$username;
     echo "password:".$password;
+
+    //create database connection
+$server = "http://fnx6frzmhxw45qcb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com/";
+$dusername = "a7vsd5p699o1mif7";
+$dpassword = "uu5y3xzmj399r0ua";
+$dbname = "m2bsi2ekjab5fc3a";
+
+$conn = new mysqli($server, $dusername, $dpassword, $dbname);
+
+if ($conn->error){
+    echo $conn->error;
+}else {
+    echo "Connected";
+}
