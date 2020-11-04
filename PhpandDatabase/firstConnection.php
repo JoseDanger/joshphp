d2616c0cbf72511480165f5f6e95da8190dc2a6c
JohnnyDanger <joshua.cramond@gmail.com>
2020-11-05 09:21:40 +1300

    new stuff

--- PhpandDatabase/firstConnection.php
+++ PhpandDatabase/firstConnection.php
@@ -22,6 +22,12 @@ $result = mysqli_query($conn, $sql);
 // show my result
 while ($row = $result->fetch_assoc()){
     echo $row["ID"].;
+    echo $row["FirstName"].;
+    echo $row["lastName"].;
+    echo $row["username"].;
+    echo $row["Password"].;
+    echo $row["address"].;
+    echo $row["phoneNumber"].;
 }
 echo "testing";
 
