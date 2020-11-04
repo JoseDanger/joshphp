f9d0278dbc73d762547b5a72485502be57a75e76
JohnnyDanger <joshua.cramond@gmail.com>
2020-11-05 10:02:28 +1300

    loginpage

--- PhpandDatabase/showAllUsers.php
+++ PhpandDatabase/showAllUsers.php
@@ -16,10 +16,9 @@
         <th>Phone</th>
 
     </tr>
-
     <?php
     //create a database connection
-    $server = "http://fnx6frzmhxw45qcb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com/";
+    $server = "http://fnx6frzmhxw45qcb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
     $dusername = "a7vsd5p699o1mif7";
     $dpassword = "uu5y3xzmj399r0ua";
     $dbname = "m2bsi2ekjab5fc3a";
@@ -40,9 +39,15 @@
 
     // show my result
     while ($row = $result->fetch_assoc()){
-        echo "<tr>"
-        echo "<td> $row[ID]. </td>"
-        echo "</tr>"
+        echo "<tr>";
+        echo "<td>".$row["id"]."</td>";
+        echo "<td>".$row["firstname"]."</td>";
+        echo "<td>".$row["lastname"]."</td>";
+        echo "<td>".$row["username"]."</td>";
+        echo "<td>".$row["password"]."</td>";
+        echo "<td>".$row["address"]."</td>";
+        echo "<td>".$row["phoneNumber"]."</td>";
+        echo "</tr>";
     }
     ?>
 </table>
