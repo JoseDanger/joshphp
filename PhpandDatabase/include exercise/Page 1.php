72844a26345cc5cdd6258e0c69fc7a29b60d11cd
JohnnyDanger <joshua.cramond@gmail.com>
2020-11-10 09:05:49 +1300

    menu and other

--- PhpandDatabase/include exercise/Page 1.php
+++ PhpandDatabase/include exercise/Page 1.php
@@ -2,6 +2,6 @@
 <ul>
     <li><a href="index.php">Home</a> </li>
     <li><a href="aboutUs.php">About Us</a> </li>
-    <li><a href="products">php>Products</a> </li>
+    <li><a href="products.php">Products</a> </li>
 
 </ul>
