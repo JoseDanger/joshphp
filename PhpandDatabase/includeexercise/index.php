39b4a8643a301c41618d84507f4d551975dd5009
JohnnyDanger <joshua.cramond@gmail.com>
2020-11-10 10:04:57 +1300

    why not

--- PhpandDatabase/includeexercise/index.php
+++ PhpandDatabase/includeexercise/index.php
@@ -11,8 +11,24 @@
     <?php
     include "Menu.php";
     ?>
-<?php include "headerimage.php"; ?>
-<div id="main"> contents</div>
+<?php include "Image.php"; ?>
+
+    <form action="recieve.php" method="get">
+    <input name="username" type="text" placeholder="Username">
+    <input name="password" type="password" placeholder="Password">
+    <input name="myNumber" type="number" placeholder="Give a number">
+    <input name="myColor" type="color" placeholder="Select a color">
+    <select name="mySelect">
+        <option value="Adult">Adult</option>
+        <option value="Child">Child</option>
+    </select>
+    <input type="submit" value="Sent">
+</form>
+
+
+</div>
+
+<div id="main"> contents 1</div>
 <div id="footer">contact information</div>
 </body>
 </html>
