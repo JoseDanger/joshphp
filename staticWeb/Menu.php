37cb1fc1784ffd5b0390824af02e62d6eac6a2f0
JohnnyDanger <joshua.cramond@gmail.com>
2020-11-17 08:07:33 +1300

    change button layout to centre and add breaks

--- staticWeb/Menu.php
+++ staticWeb/Menu.php
@@ -1,13 +1,14 @@
-
+<center
 <ul>
-    <li><button><b></button><a href="loginForm.html">LOG IN</a></b> </li>
-    <li><button><b></b><a href="About%20Us.html">ABOUT US</a></b></button> </li>
+    <li><button><b><a href="loginForm.html">LOG IN</a>
+            </b></button> </li> <br>
+    <li><button><b></b><a href="About%20Us.html">ABOUT US</a></b></button> </li><br>
     <li><button><b><a href="phpForm_Post.html">REGISTER ACCOUNT</a></b></button> </li>
-    <li><button><b></b><a href="">YOUR ACCOUNT</a></b></button> </li>
-    <li><button><b></b><a href="Cart.html">CART/CHECKOUT</a></b></button> </li>
-    <li><button><b></b><a href="">WISHLIST</a></b></button> </li>
+    <br><li><button><b></b><a href="">YOUR ACCOUNT</a></b></button> </li><br>
+    <li><button><b></b><a href="Cart.html">CART/CHECKOUT</a></b></button> </li><br>
+    <li><button><b></b><a href="">WISHLIST</a></b></button> </li><br>
 </ul>
-
+</centre>
 
 
 <br><br>
