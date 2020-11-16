45cc38d0746e4f4ebd7ac6b65b2bc7771a381bee
JohnnyDanger <joshua.cramond@gmail.com>
2020-11-17 08:03:46 +1300

    change login page buttons

--- staticWeb/Menu.php
+++ staticWeb/Menu.php
@@ -1,7 +1,13 @@
 
 <ul>
-    <li><a href="index.php">Home</a> </li>
-    <li><a href="aboutUs.php">About Us</a> </li>
-    <li><a href="products.php">Products</a> </li>
-
+    <li><button><b></button><a href="loginForm.html">LOG IN</a></b> </li>
+    <li><button><b></b><a href="About%20Us.html">ABOUT US</a></b></button> </li>
+    <li><button><b><a href="phpForm_Post.html">REGISTER ACCOUNT</a></b></button> </li>
+    <li><button><b></b><a href="">YOUR ACCOUNT</a></b></button> </li>
+    <li><button><b></b><a href="Cart.html">CART/CHECKOUT</a></b></button> </li>
+    <li><button><b></b><a href="">WISHLIST</a></b></button> </li>
 </ul>
+
+
+
+<br><br>
