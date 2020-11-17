a04ce61bb93353fbe22145bcbb0493054bd2b28e
JohnnyDanger <joshua.cramond@gmail.com>
2020-11-18 09:23:15 +1300

    do the same for all pages

--- staticWeb/Addacart.php
+++ staticWeb/Addacart.php
@@ -8,28 +8,28 @@ $qty = $_POST["qty"];
 // if i dont have an ordered product list in my session, i will create one.
 if (isset($_SESSION["orderedProductIDs"])){
     //if something has been added to our shopping cart
-    $orderedProductIDs = ["orderedProductIDs"];
-    $orderedProductQtys = ["orderedProductQtys"];
-    if (in_array($productID, $orderedProductIDs)){
-        $index = array_search($productID, $orderedProductIDs);
-        $orderedProductQtys[$index] + $qty;
+    $orderedProductID = ["orderedProductIDs"];
+    $orderedProductQty = ["orderedProductQtys"];
+    if (in_array($productID, $orderedProductID)){
+        $index = array_search($productID, $orderedProductID);
+        $orderedProductQty[$index] + $qty;
         else
             //this situation only happens if there is none of the same product in our shopping cart
-        array_push($orderedProductIDs, $productID); //add a new product into the array.
-        array_push($orderedProductQtys. $qty);
+        array_push($orderedProductID, $productID); //add a new product into the array.
+        array_push($orderedProductQty. $qty);
     }
 
 
 }else{
     // if nothing in our shopping cart yet
-    $orderedProductIDs = [];
-    $orderedProductQtys = [];
-    array_push($orderedproductIDs, $productID);//append one at bottom of the array.
-    array_push($orderedproductIDs, $qty);
+    $orderedProductID = [];
+    $orderedProductQty = [];
+    array_push($orderedproductID, $productID);//append one at bottom of the array.
+    array_push($orderedproductID, $qty);
 }
 //put the new ordered product lists back to session variable
-$_SESSION["orderedProductIDs"] = $orderedProductIDs;
-$_SESSION["orderedProductIDs"] = $orderedProductQtys;
+$_SESSION["orderedProductIDs"] = $orderedProductID;
+$_SESSION["orderedProductIDs"] = $orderedProductQty;
 
 //Go back to previous page.
 header( 'Location:' . $_SERVER['HTTP_REFERER']);
