bcbd25bc4d670ab055a00543611331c901453b0f
JohnnyDanger <joshua.cramond@gmail.com>
2020-11-18 08:52:21 +1300

    doesn't work

--- staticWeb/index.php
+++ staticWeb/index.php
@@ -13,7 +13,7 @@
 
 <div>
     <button><b><a href="loginForm.html">LOG IN </a></b></button>
-    <button><img id="searchImage" width="25px" src="images/Cart etc/search-icon-large.jpg.png"><p> </p>
+    <button><img id="searchImage" width="25px" src="images/Cart etc/search-icon-large.jpg.png">
         <b>SEARCH</b></Button>
     <Button><b>REGISTER ACCOUNT</b></Button>
     <br><br>
