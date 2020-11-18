ac60ea8db74c8117429ec52c2d55784658ce0937
JohnnyDanger <joshua.cramond@gmail.com>
2020-11-19 09:32:29 +1300

    footer centred

--- staticWeb/Products.php
+++ staticWeb/Products.php
@@ -15,9 +15,9 @@ $conn = new mysqli($server, $dbusername, $dbpassword, $dbname);
 // take input from selected category;
 if (isset($_GET["Category"])){
     echo "<h1>".$_GET["Category"]."</h1>";
-    $sql = "select * from products where category = ".$_GET["Category"];
+    $sql = "select * from Products where Category = ".$_GET["Category"];
 }else{
-    $sql = "select * from products";
+    $sql = "select * from Products";
 }
 
 
