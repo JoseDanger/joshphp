44dc17f13817573d7920c37af2c8438600d5d695
JohnnyDanger <joshua.cramond@gmail.com>
2020-11-19 09:18:54 +1300

    check div in home page

--- staticWeb/index.php
+++ staticWeb/index.php
@@ -1,121 +1,122 @@
 <!DOCTYPE html>
 <html lang="en">
 
-    <meta charset="UTF-8">
-    <title>PET FOODS</title>
-    <link rel="stylesheet" href="css/style.css">
-    <script src="js/script.js"></script>
+<meta charset="UTF-8">
+<title>PET FOODS</title>
+<link rel="stylesheet" href="css/style.css">
+<script src="js/script.js"></script>
 
 <body>
 <centre>
 
-<h1><B>PET FOODS</B></h1><br>
-<p></p>
-<div width="100%" style="text-align: center">
-
-    <button><b><a href="Login.php">LOGIN </a></b></button>
-    <Button><b><a href="Registrationform.php">REGISTER ACCOUNT</b></Button>
-    <Button><b><a href="Login.php"> YOUR ACCOUNT</b></a></Button><br><br>
-    <Button><img id="cartImage" width="25px" src="images/Cart etc/Cart.jpg"><a href="Cart.php"><b>
-                CART/CHECKOUT</b></Button>
-
-
-
-    <br><br>
-</div>
-
-
+    <h1><B>PET FOODS</B></h1><br>
+    <p></p>
     <div width="100%" style="text-align: center">
 
-            <img src="images/MainImages/main.jpg" width = 500px border=10 />
+        <button><b><a href="Login.php">LOGIN </a></b></button>
+        <Button><b><a href="Registrationform.php">REGISTER ACCOUNT</b></Button>
+        <Button><b><a href="Login.php"> YOUR ACCOUNT</b></a></Button>
+        <br><br>
+        <Button><img id="cartImage" width="25px" src="images/Cart etc/Cart.jpg"><a href="Cart.php"><b>
+                    CART/CHECKOUT</b></Button>
+
+
         <br><br>
     </div>
 
 
-<button><b><a href="AboutUs.html"> ABOUT US</a> </b></button>
+    <div width="100%" style="text-align: center">
 
-    <br><br><br>
+        <img src="images/MainImages/main.jpg" width=500px border=10/>
+        <br><br>
 
 
+        <button><b><a href="AboutUs.html"> ABOUT US</a> </b></button>
 
+        <br><br><br>
+
+
+        <br>
+
+        <h1>STAFF PICKS 20% Off!</b></h1>
+        <br>
+
+        <img src="images/dog/Picture2.jpg" height="303" width="302" border="7px"/><br>
+        <h3><b>Pedigree Chicken With Rice: was $30.99 <br> <u>now</u> <br>
+                <h2><b></b>$24.79</b></h2></b></h3>
+        <br><br><br>
+        <img src="images/Cat/5b7b244e230c48.80505224.png" height="303" width="302" border="7px"/><br>
+
+        <h3><b> Omega Plus: New Zealand Salmon: was $24.99 <br> <u>now</u> <br>
+                <h2><b>$19.00</b></h2></b></h3>
+
+        <br><br><br>
+        <Button><a href="index.php"><b>BACK TO TOP</b></a></Button>
+        </center>
+        <br>
+        <br>
+        <br>
+    </div>
+
+    <center>
+        <footer>
+            <b><p>Copyright &copy; 2020, ALL RIGHTS RESERVED</p></b>
             <br>
-
-           <h1>STAFF PICKS  20% Off!</b></h1>
-                <br>
-
-                <img src="images/dog/Picture2.jpg" height="303" width="302" border = "7px"/><br>
-                <h3><b>Pedigree Chicken With Rice: was $30.99 <br> <u>now</u> <br><h2><b></b>$24.79</b></h2></b></h3>
-                <br><br><br>
-                <img src="images/Cat/5b7b244e230c48.80505224.png" height="303" width="302" border = "7px"/><br>
-
-                <h3><b> Omega Plus: New Zealand Salmon: was $24.99 <br> <u>now</u> <br><h2><b>$19.00</b></h2></b></h3>
-
-<br><br><br> <Button><a href="index.php"><b>BACK TO TOP</b></a></Button></center>
-            <br>
-            <br>
-            <br>
-
-
-           <center> <footer>
-                <b><p>Copyright &copy; 2020, ALL RIGHTS RESERVED</p></b>
-                <br>
-                <b><p><div width="100%" style="text-align: center">
+            <b><p>
+                <div width="100%" style="text-align: center">
                     <table>
-                    <tr>
-                        <td>
-                            Health Food <br>
-                            Best Products <br>
-                            Pet Food Brands <br>
-                            Join Our Team <br>
-                            Our Donations <br>
-                        </td>
-                        <br>
-                        <td>
-                            Donate To Dogs In Need <br>
-                            Re-Home Your Dog <br>
-                            Get A Re-Homed Dog (Save A Life) <br>
-                            Contact Us <br>
-                            Train Your Pet <br>
+                        <tr>
+                            <td>
+                                Health Food <br>
+                                Best Products <br>
+                                Pet Food Brands <br>
+                                Join Our Team <br>
+                                Our Donations <br>
+                            </td>
+                            <br>
+                            <td>
+                                Donate To Dogs In Need <br>
+                                Re-Home Your Dog <br>
+                                Get A Re-Homed Dog (Save A Life) <br>
+                                Contact Us <br>
+                                Train Your Pet <br>
 
-                        </td>
-                    </tr>
-                    </table></centre>
-                </p></b>
-            </footer></centre></div>
-
-
-
-
-            <!--bold text: <strong>-->
-            <!--underline: <em> -->
-            <!--itallic: <u> -->
-            <!--small: <small>-->
-            <!--highlight <mark>-->
-            <!--subscript: <sub> lowers text-->
-            <!--superscipt <sup> highers text-->
-            <!-- line Breaks: self closing anduse <br>-->
-            <!-- highlight: <mark>-->
-            <!--lines to seperate your contect: <hr>-->
-            <!-- grouping elements <div> -->
-            <!--attributes CLasses andID,s: identify a group of elements for multiple purposes-->
-            <!--tables: <Table> -->
-            <!-- table headings: <thead>-->
-            <!-- table row: <tr> -->
-            <!-- table data underneath: <td> -->
-            <!-- butttons: <button>-->
-
-            <!-- Form Select List -->
-            <!-- Form: <Form>-->
-            <!-- Lable: <Lable>-->
-            <!-- link to home page:
-                                    <nav>
-                                            <ul>
-                                                <li><a href="index.php">Home</a><li>
-                                            </ul>
-                                    <nav>
-            -->
+                            </td>
+                        </tr>
+                    </table>
+</centre>
+</p></b>
+</footer></centre></div>
 
 
+<!--bold text: <strong>-->
+<!--underline: <em> -->
+<!--itallic: <u> -->
+<!--small: <small>-->
+<!--highlight <mark>-->
+<!--subscript: <sub> lowers text-->
+<!--superscipt <sup> highers text-->
+<!-- line Breaks: self closing anduse <br>-->
+<!-- highlight: <mark>-->
+<!--lines to seperate your contect: <hr>-->
+<!-- grouping elements <div> -->
+<!--attributes CLasses andID,s: identify a group of elements for multiple purposes-->
+<!--tables: <Table> -->
+<!-- table headings: <thead>-->
+<!-- table row: <tr> -->
+<!-- table data underneath: <td> -->
+<!-- butttons: <button>-->
+
+<!-- Form Select List -->
+<!-- Form: <Form>-->
+<!-- Lable: <Lable>-->
+<!-- link to home page:
+                        <nav>
+                                <ul>
+                                    <li><a href="index.php">Home</a><li>
+                                </ul>
+                        <nav>
+-->
 
 
 </body>
