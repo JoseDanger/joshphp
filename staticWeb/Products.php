69f53f17b4a333dd180242bdaf67e597d4fcd329
JohnnyDanger <joshua.cramond@gmail.com>
2020-11-19 10:11:15 +1300

    change function to put name on top of right page

--- staticWeb/Products.php
+++ staticWeb/Products.php
@@ -45,8 +45,8 @@ while ($row = $result->fetch_assoc()) {
 
 function createDatabaseConnection(){
     //1. connect to database
-    $server = "http://fnx6frzmhxw45qcb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-    $dbusername = " a7vsd5p699o1mif7";
+    $server = "fnx6frzmhxw45qcb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $dbusername = "a7vsd5p699o1mif7";
     $dbpassword = "uu5y3xzmj399r0ua";
     $dbname = "m2bsi2ekjab5fc3a";
 
