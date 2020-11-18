fce1acf5eff883659b73fb09faf794d840e21a99
JohnnyDanger <joshua.cramond@gmail.com>
2020-11-18 17:45:21 +1300

    design colours

--- staticWeb/Menu.php
+++ staticWeb/Menu.php
@@ -7,15 +7,15 @@
     <script src="js/script.js"></script>
 </head>
 <body>
-<center
+<center>
 <ul>
-    <li><button><b><a href="Old Page Design/loginForm.html">LOG IN</a>
+    <li><button><b><a href=Login.php">LOG IN</a>
             </b></button> </li> <br>
     <li><button><b></b><a href="AboutUs.html">ABOUT US</a></b></button> </li><br>
-    <li><button><b><a href="Old Page Design/phpForm_Post.html">REGISTER ACCOUNT</a></b></button> </li>
-    <br><li><button><b></b><a href="">YOUR ACCOUNT</a></b></button> </li><br>
+    <li><button><b><a href="Registrationform.php">REGISTER ACCOUNT</a></b></button> </li>
+    <br><li><button><b></b><a href="Registrationpage.php">YOUR ACCOUNT</a></b></button> </li><br>
     <li><button><b></b><a href="Cart.php">CART</a></b></button> </li><br>
-    <li><button><b></b><a href="">WISHLIST</a></b></button> </li><br>
+
     <?php
           if (isset($_SESSION["userID"])){
         ?>
@@ -25,11 +25,11 @@
     ?>
 
 </ul>
-</centre>
+
 <button><b><a href="Menu.php"> BACK TO TOP</a> </b></button>
 <Button><b><a href="index.php">HOME PAGE</a></b></Button>
 <br><br><br><br>
-</body>
+</body></center>
 <footer>
     <b><p>Copyright &copy; 2020, ALL RIGHTS RESERVED</p></b>
     <br>
