5f1be9e140fe96e5f31e28cb0f0c712918710c5e
JohnnyDanger <joshua.cramond@gmail.com>
2020-11-19 08:56:17 +1300

    fix database connection

--- staticWeb/Login.php
+++ staticWeb/Login.php
@@ -18,7 +18,7 @@ if (isset($_POST["username"]) && isset($_POST["password"])) {
     //1. create a database connection
     $server = "http://fnx6frzmhxw45qcb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
     $dbusername = " a7vsd5p699o1mif7";
-    $dbpassword = "fdavedw769oxw5pd";
+    $dbpassword = "uu5y3xzmj399r0ua";
     $dbname = "k2nfay1osz1i59kc";
 
     $conn = new mysqli($server, $dbusername, $dbpassword, $dbname);
--- staticWeb/checkout.php
+++ staticWeb/checkout.php
@@ -31,7 +31,7 @@ function createDatabaseConnection(){
     //1. connect to database
     $server = "http://fnx6frzmhxw45qcb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
     $dbusername = " a7vsd5p699o1mif7";
-    $dbpassword = " a7vsd5p699o1mif7";
+    $dbpassword = "uu5y3xzmj399r0ua";
     $dbname = "k2nfay1osz1i59kc";
 
     $conn = new mysqli($server, $dbusername, $dbpassword, $dbname);
