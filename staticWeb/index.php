ca5ce5e50763082f1ebdbb1fe38a24463ed0cbca
JohnnyDanger <joshua.cramond@gmail.com>
2020-11-18 17:38:13 +1300

    design colours

--- staticWeb/index.php
+++ staticWeb/index.php
@@ -12,14 +12,12 @@
 <h1><B>PET FOODS</B></h1><br>
 <p></p>
 
-<div>
     <button><b><a href="Login.php">LOGIN </a></b></button>
     <Button><b><a href="Registrationform.php">REGISTER ACCOUNT</b></Button>
-
     <Button><b><a href="Login.php"> YOUR ACCOUNT</b></a></Button><br><br>
     <Button><img id="cartImage" width="25px" src="images/Cart etc/Cart.jpg"><a href="Cart.php"><b>
                 CART/CHECKOUT</b></Button>
-</div>
+
 
 
     <br><br>
