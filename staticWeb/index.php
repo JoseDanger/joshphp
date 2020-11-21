70bbdf1443d46537f709402e900ac0c3103d4353
JohnnyDanger <joshua.cramond@gmail.com>
2020-11-21 18:45:00 +1300

    putting menu onto the home page

--- staticWeb/index.php
+++ staticWeb/index.php
@@ -31,7 +31,25 @@
         <br><br>
 
 
-        <button><b><a href="AboutUs.html"> ABOUT US</a> </b></button>
+        <h1><b>MENU</b></h1><br>
+        <img src="images/MainImages/menu.png"border=10 width="500px" border=10></div><br><br>
+    <div width="100%" style="text-align: center">
+        <button><b><a href=Login.php">LOG IN</a></button><br><br>
+        <button><b><a href="Registrationpage.php">YOUR ACCOUNT</a></b></button>
+        <button><b><a href="Registrationform.php">REGISTER ACCOUNT</a></b></button><br><br>
+        <button><b><a href="Cart.php">CART</a></b></button>
+        <Button><b><a href="index.php">HOME PAGE</a></b></Button>
+        <button><b><a href="AboutUs.html">ABOUT US</a></b></button>
+
+
+        <?php
+        if (isset($_SESSION["userID"])){
+            ?>
+            <button><b></b><a href="">MY ORDERS</a></b></button>
+            <?php
+        }
+        ?>
+
 
         <br><br><br>
 
