be4b8a546517954dbc3fffbe2e235788b4489d21
JohnnyDanger <joshua.cramond@gmail.com>
2020-11-24 00:19:47 +1300

    added footer include

--- staticWeb/HomePage.php
+++ staticWeb/HomePage.php
@@ -55,35 +55,9 @@
 
 
 
-<center>
-    <footer>
-        <b><p>Copyright &copy; 2020, ALL RIGHTS RESERVED</p></b>
-        <br>
-        <b><p>
-            <div width="100%" style="text-align: center">
-                <table width="100%">
-                    <tr>
-                        <td width="50%">
-                            Health Food <br>
-                            Best Products <br>
-                            Pet Food Brands <br>
-                            Join Our Team <br>
-                            Our Donations <br>
-                        </td>
-                        <br>
-                        <td>
-                            Donate To Dogs In Need <br>
-                            Re-Home Your Dog <br>
-                            Get A Re-Homed Dog (Save A Life) <br>
-                            Contact Us <br>
-                            Train Your Pet <br>
-
-                        </td>
-                    </tr>
-                </table>
-
-            </p></b>
-    </footer></centre></div>
+<div id="FooterDiv">
+    <?php include "footer.php"; ?>
+</div>
 
 
     <!--bold text: <strong>-->
