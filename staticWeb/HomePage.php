f4a751039d3aea9960119042d7645b04d4b20d1c
JohnnyDanger <joshua.cramond@gmail.com>
2020-11-24 00:25:20 +1300

    keep product options on new home pae

--- staticWeb/HomePage.php
+++ staticWeb/HomePage.php
@@ -13,6 +13,9 @@
     <br><br>
     <Button><img id="cartImage" width="25px" src="images/Cart etc/Cart.jpg"><a href="Cart.php"><b>
                 CART</b></Button>
+    <div id="categoryDiv">
+        <?php include "category.php"; ?>
+    </div>
 
 
     <br><br>
