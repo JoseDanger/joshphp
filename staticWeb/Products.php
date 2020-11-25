77cf29bd9f44d5d87688514f2d8c6cf3c2f880d7
JohnnyDanger <joshua.cramond@gmail.com>
2020-11-26 00:37:13 +1300

    try fix cart

--- staticWeb/Products.php
+++ staticWeb/Products.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-<meta charset="UTF-8">
 <link rel="stylesheet" href="css/style.css">
 <script src="js/script.js"></script>
+<meta charset="UTF-8">
+<title>PET FOODS</title>
+
 <title>PET FOODS</title>
 
 <div width="100%" style="text-align: center">
@@ -10,15 +12,12 @@
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
-
     //1. connect to database
     $server = "fnx6frzmhxw45qcb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
     $dbusername = "a7vsd5p699o1mif7";
     $dbpassword = "uu5y3xzmj399r0ua";
     $dbname = "m2bsi2ekjab5fc3a";
-
     $conn = new mysqli($server, $dbusername, $dbpassword, $dbname);
-
     //2. create a query
     // take input from selected category;
     if (isset($_GET["category"])) {
@@ -26,22 +25,17 @@
         echo "<img src='$photo' width='500px' border = '10px'>";
         $categoryName = getCategoryNameFromCategoryID($_GET["category"]);
         echo "<h1>".$categoryName . "</h1>";
-
         $sql = "select * from PRODUCT where Category = " . $_GET["category"];
     } else {
         $sql = "select * from PRODUCT";
     }
-
-
     //3. run the query on that connection
     $result = mysqli_query($conn, $sql);
-
     //4. show result
     while ($row = $result->fetch_assoc()) {
         ?>
         <div>
             <p><?php echo $row["ProductName"]; ?></p>
-
             <p><img src="<?php echo $row["image"]; ?>"</p>
             <p>$<?php echo $row["PricePerUnit"]; ?></p>
             <form action="Addtocart.php" method="post">
@@ -52,14 +46,12 @@
         </div>
         <?php
     }
-
     function createDatabaseConnection(){
         //1. connect to database
         $server = "fnx6frzmhxw45qcb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
         $dbusername = "a7vsd5p699o1mif7";
         $dbpassword = "uu5y3xzmj399r0ua";
         $dbname = "m2bsi2ekjab5fc3a";
-
         $conn = new mysqli($server, $dbusername, $dbpassword, $dbname);
         return $conn;
     }
@@ -69,10 +61,8 @@
         $conn = createDatabaseConnection();
         //2. query
         $sql = "select Photo from Category where ID=$CategoryID";
-
         //3. run query
         $result = mysqli_query($conn, $sql);
-
         //4. show result
         while ($row = $result->fetch_assoc()) {
             $Photo = $row["Photo"];
@@ -90,10 +80,8 @@
         $conn = createDatabaseConnection();
         //2. query
         $sql = "select Name from Category where ID=$CategoryID";
-
         //3. run query
         $result = mysqli_query($conn, $sql);
-
         //4. show result
         while ($row = $result->fetch_assoc()) {
             $name = $row["Name"];
