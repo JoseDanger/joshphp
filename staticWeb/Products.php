56d89792e44f592ec551452a91ccbf63cabc3451
JohnnyDanger <joshua.cramond@gmail.com>
2020-11-26 00:30:05 +1300

    try fix cart

--- staticWeb/Products.php
+++ staticWeb/Products.php
@@ -1,133 +1,111 @@
 <!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>CATEGORY</title>
-    <link rel="stylesheet" href="css/style.css">
-    <script src="js/script.js"></script>
-</head><div width="100%" style="text-align: center">
-    <h1><b>CART</b></h1><br>
-    <img src="images/MainImages/Cart.jpg"border=10 width="500px" border=10><br><br><br>
+<html lang="en">
+<meta charset="UTF-8">
+<link rel="stylesheet" href="css/style.css">
+<script src="js/script.js"></script>
+<title>PET FOODS</title>
 
-    <h2>If you're purchase is over $300 DELIVERY IS FREE!</h2>
+<div width="100%" style="text-align: center">
     <?php
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
-    @session_start();
-    $orderedProductIDs = $_SESSION["orderedProductIDs"];
-    $orderedProductQtys = $_SESSION["orderedProductQtys"];
 
-    $delivery = 15;
-    $i = 0;
-    $total = 0;
-    while ($i<sizeof($orderedProductIDs)){
-        $orderedProductID = $orderedProductIDs[$i];
-        $orderedProductQty = $orderedProductQtys[$i];
-        $productName = getProductNameByProductID($orderedProductID);
-        $price=getProductPriceByProductID($orderedProductID);
-        echo "<p>Name: $productName Qty: $orderedProductQty Unit Price: $price
-         Price: ".($price*$orderedProductQty)."</p>";
-        $total = $total + ($price*$orderedProductQty);
-        $i++;
+    //1. connect to database
+    $server = "fnx6frzmhxw45qcb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $dbusername = "a7vsd5p699o1mif7";
+    $dbpassword = "uu5y3xzmj399r0ua";
+    $dbname = "m2bsi2ekjab5fc3a";
+
+    $conn = new mysqli($server, $dbusername, $dbpassword, $dbname);
+
+    //2. create a query
+    // take input from selected category;
+    if (isset($_GET["category"])) {
+        $photo = getCategoryPhotoFromCategoryID($_GET["category"]);
+        echo "<img src='$photo' width='500px' border = '10px'>";
+        $categoryName = getCategoryNameFromCategoryID($_GET["category"]);
+        echo "<h1>".$categoryName . "</h1>";
+
+        $sql = "select * from PRODUCT where Category = " . $_GET["category"];
+    } else {
+        $sql = "select * from PRODUCT";
     }
-    ?>
-    <p>Total: $<?php echo $total; ?></p>
-    <?php
-    //if ($total > ???){
-    //
-    //}else{
-    //
-    //}(Give Message for Either)
-    if ($total > 300){ $total = $total;
-
-    }else{ $total =$total + $delivery;};
 
 
-    if (isset($_SESSION["userID"])){
+    //3. run the query on that connection
+    $result = mysqli_query($conn, $sql);
+
+    //4. show result
+    while ($row = $result->fetch_assoc()) {
         ?>
+        <div>
+            <p><?php echo $row["ProductName"]; ?></p>
 
-        <form action="checkout.php" method="post">
-            <textarea name="ShippingAddress" rows="10" cols="50"></textarea>
-            <input type="submit" value="CheckOut"
-        </form>
+            <p><img src="<?php echo $row["image"]; ?>"</p>
+            <p>$<?php echo $row["PricePerUnit"]; ?></p>
+            <form action="Addtocart.php" method="post">
+                <input name="productID" value="<?php echo $row["ID"]; ?>" type="hidden">
+                <input name="qty" type="number" placeholder="QTY" min="0">
+                <input type="submit" value="Add to cart">
+            </form>
+        </div>
         <?php
-    }else{
-        echo "you need to login";
     }
+
     function createDatabaseConnection(){
         //1. connect to database
         $server = "fnx6frzmhxw45qcb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
         $dbusername = "a7vsd5p699o1mif7";
         $dbpassword = "uu5y3xzmj399r0ua";
         $dbname = "m2bsi2ekjab5fc3a";
+
         $conn = new mysqli($server, $dbusername, $dbpassword, $dbname);
         return $conn;
     }
-    /**
-     * @name getProductNameByProductID
-     * @param $productID
-     * @return product name
-     */
-    function getProductNameByProductID($productID){
+    function getCategoryPhotoFromCategoryID($CategoryID)
+    {
         //1. create a db connection
         $conn = createDatabaseConnection();
         //2. query
-        $sql = "select ProductName from PRODUCT where ID=$productID";
+        $sql = "select Photo from Category where ID=$CategoryID";
+
         //3. run query
         $result = mysqli_query($conn, $sql);
+
         //4. show result
-        while ($row = $result->fetch_assoc()){
-            $name = $row["ProductName"];
+        while ($row = $result->fetch_assoc()) {
+            $Photo = $row["Photo"];
+        }
+        return $Photo;
+    }
+    /**
+     * @name getCategoryNameFromCategoryID
+     * @param $CategoryID
+     * @return $name
+     */
+    function getCategoryNameFromCategoryID($CategoryID)
+    {
+        //1. create a db connection
+        $conn = createDatabaseConnection();
+        //2. query
+        $sql = "select Name from Category where ID=$CategoryID";
+
+        //3. run query
+        $result = mysqli_query($conn, $sql);
+
+        //4. show result
+        while ($row = $result->fetch_assoc()) {
+            $name = $row["Name"];
         }
         return $name;
     }
-    /**
-     * @name getProductPriceByProductID
-     * @param $productID
-     * @return product price
+    /*
+     * @Name trying to put pet food into table
+     * @param $
      */
-    function getProductPriceByProductID($productID){
-        //1. create a db connection
-        $conn = createDatabaseConnection();
-        //2. query
-        $sql = "select PricePerUnit from PRODUCT where ID=$productID";
-        //3. run query
-        $result = mysqli_query($conn, $sql);
-        //4. show result
-        while ($row=$result->fetch_assoc()){
-            $price = $row["PricePerUnit"];
-        }
-        return $price;
-    }
-    ?><br><br>
-    <button><b><a href="Cart.php"> BACK TO TOP</a> </b></button>
-    <button><b><a href="Login.php"> LOGIN</a> </b></button>
-    <Button><b><a href="HomePage.php">HOME PAGE</a></b></Button>
-    <br><br><br><br>
-    </body>
-    <footer>
-        <b><p>Copyright &copy; 2020, ALL RIGHTS RESERVED</p></b>
-        <br>
-        <b><p>
-            <table>
-                <tr>
-                    <ul>
-                        Health Food <br>
-                        Best Products <br>
-                        Pet Food Brands <br>
-                        Join Our Team <br>
-                        Our Donations <br>
-                    </ul>
-                    <ul>
-                        Donate To Dogs In Need <br>
-                        Re-Home Your Dog <br>
-                        Get A Re-Homed Dog (Save A Life) <br>
-                        Contact Us <br>
-                        Train Your Pet <br>
-                    </ul>
-                </tr>
-            </table>
-            </p></b>
-    </footer></center></div>
-</html>
+    ?>
+    <div id="FooterDiv">
+        <?php include "footer.php"; ?>
+    </div>
+</html
