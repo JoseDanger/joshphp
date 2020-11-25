25b1bf68279e003341376477d45040aab74ff406
JohnnyDanger <joshua.cramond@gmail.com>
2020-11-25 23:44:51 +1300

    get backup code from github to get photo and products working again

--- staticWeb/Products.php
+++ staticWeb/Products.php
@@ -4,20 +4,17 @@
 <link rel="stylesheet" href="css/style.css">
 <script src="js/script.js"></script>
 <title>PET FOODS</title>
-
 <div width="100%" style="text-align: center">
-    <button><b><a href="HomePage.php"> HOME PAGE </a> </b></button>
-    <button><b><a href="Cart.php"> CART </a> </b></button>
 <?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
 //1. connect to database
 $server = "fnx6frzmhxw45qcb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 $dbusername = "a7vsd5p699o1mif7";
 $dbpassword = "uu5y3xzmj399r0ua";
 $dbname = "m2bsi2ekjab5fc3a";
+
 
 $conn = new mysqli($server, $dbusername, $dbpassword, $dbname);
 
@@ -30,20 +27,18 @@ if (isset($_GET["category"])) {
     echo "<h1>".$categoryName . "</h1>";
 
     $sql = "select * from PRODUCT where Category = " . $_GET["category"];
+    echo $sql;
+    //echo $sql;
 } else {
     $sql = "select * from PRODUCT";
 }
-
-
 //3. run the query on that connection
 $result = mysqli_query($conn, $sql);
-
 //4. show result
 while ($row = $result->fetch_assoc()) {
-    ?>
+} ?>
     <div>
         <p><?php echo $row["ProductName"]; ?></p>
-
         <p><img src="<?php echo $row["image"]; ?>"</p>
         <p>$<?php echo $row["PricePerUnit"]; ?></p>
         <form action="Addtocart.php" method="post">
@@ -52,62 +47,3 @@ while ($row = $result->fetch_assoc()) {
             <input type="submit" value="Add to cart">
         </form>
     </div>
-    <?php
-}
-
-function createDatabaseConnection(){
-    //1. connect to database
-    $server = "fnx6frzmhxw45qcb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-    $dbusername = "a7vsd5p699o1mif7";
-    $dbpassword = "uu5y3xzmj399r0ua";
-    $dbname = "m2bsi2ekjab5fc3a";
-
-    $conn = new mysqli($server, $dbusername, $dbpassword, $dbname);
-    return $conn;
-}
-function getCategoryPhotoFromCategoryID($CategoryID)
-{
-    //1. create a db connection
-    $conn = createDatabaseConnection();
-    //2. query
-    $sql = "select Photo from Category where ID=$CategoryID";
-
-    //3. run query
-    $result = mysqli_query($conn, $sql);
-
-    //4. show result
-    while ($row = $result->fetch_assoc()) {
-        $Photo = $row["Photo"];
-    }
-    return $Photo;
-}
-/**
- * @name getCategoryNameFromCategoryID
- * @param $CategoryID
- * @return $name
- */
-function getCategoryNameFromCategoryID($CategoryID)
-{
-    //1. create a db connection
-    $conn = createDatabaseConnection();
-    //2. query
-    $sql = "select Name from Category where ID=$CategoryID";
-
-    //3. run query
-    $result = mysqli_query($conn, $sql);
-
-    //4. show result
-    while ($row = $result->fetch_assoc()) {
-        $name = $row["Name"];
-    }
-    return $name;
-}
-/*
- * @Name trying to put pet food into table
- * @param $
- */
- ?>
-<div id="FooterDiv">
-    <?php include "footer.php"; ?>
-</div>
-</html
